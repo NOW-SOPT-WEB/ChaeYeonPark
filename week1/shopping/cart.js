85fe8be99dd7e6ae17a9d3f74e65278a03bec665
chaeyeon <ccyy1029@naver.com>
2024-04-17 12:49:29 +0900

    feat : checkbox 전체 선택

--- week1/shopping/cart.js
+++ week1/shopping/cart.js
@@ -8,19 +8,38 @@ let cartProductCard = cartProducts.map(product => {
 
     return `
     <tr>
-    <td><input type="checkbox"></td>
+    <td><input type="checkbox" class="checkbox"></td>
     <td><img src="${product.image}" style="width: 3rem;"></td>
     <td>${product.name}</td>
     <td>${product.price}</td>
     <td>${product.category}</td>
-    <td><button>삭제</button></td>
+    <td><button class="button">삭제</button></td>
     </tr>
     `
 });
 
 tableClass.innerHTML += cartProductCard.join('');
-//+= 하니까 덮어 써지지 않음.
+//+= 하니까 th 덮어 써지지 않음.
 
 //버튼 클릭하면 삭제
+const deleteBtns = document.querySelectorAll('.button');
+deleteBtns.forEach((deleteBtn, index) => {
+    deleteBtn.addEventListener('click', () => {
+        console.log(index);
+        //localStorage.removeItem('cartproducts');
+    });
+});
+
+
+
+//체크 전체
+const checkboxAll = document.querySelector('.all')
+const checkboxs = document.querySelectorAll('.checkbox');
+
+checkboxAll.addEventListener('click', ()=>{
+    checkboxs.forEach(checkbox => {
+        checkbox.checked = checkboxAll.checked;
+    });
+});
 
 //구매하기 버튼 (체크 구현)
