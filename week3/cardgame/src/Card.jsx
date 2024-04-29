f38026768d69d4385ed69366162b2c0cf50ba987
chaeyeon <ccyy1029@naver.com>
2024-04-30 01:43:29 +0900

    feat: 카드 매칭 기능 구현

--- week3/cardgame/src/Card.jsx
+++ week3/cardgame/src/Card.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import CardGame from "./CardGame";
 
 
-const Card = ({image, onClick}) => {
-    const [fliped, setFliped] = useState(false);
+const Card = ({image, onClick, isFlipped}) => {
+    const [flipped, setFlipped] = useState(false);
+
+    // useEffect(() => {
+    //     // 상위 컴포넌트로부터 isFlipped props가 변경될 때마다 flipped 상태 업데이트
+    //     setFlipped(isFlipped);
+    // }, [isFlipped]);
 
     const handleClick = () => {
-        setFliped(true);
-        onClick();
+        if (!flipped) {
+            // 이미 뒤집힌 카드가 아닌 경우에만 클릭 이벤트 처리
+            setFlipped(true);
+            onClick();
+        }
     };
 
     return(
         <CardWrapper className="card" onClick={handleClick}>
-            {fliped ? <ImgWrapper src={image} alt="true"></ImgWrapper> : <ImgWrapper src="/animal.jpg" alt="false"></ImgWrapper>}
+            {flipped 
+            ? <ImgWrapper src={image} alt="true" />
+            : <ImgWrapper src="/animal.jpg" alt="false" />}
         </CardWrapper>
     );
 };
--- week3/cardgame/src/CardGame.jsx
+++ week3/cardgame/src/CardGame.jsx
@@ -7,22 +7,51 @@ import styled from "@emotion/styled";
 const CardGame = () => {
     const [cards1, setCards1] = useState(shuffleArray([...Array(5).keys()]));
     const [cards2, setCards2] = useState(shuffleArray([...Array(5).keys()]));
+    let CardClickCount = 0;
+    let CardMatchCount = 0;
+    let CardNum1 = -1;
+    let CardNum2 = -1;
 
-    const handleCardClick = (index) => {
-        
+    const handleCardClick = (card) => {
+        if (CardNum1 === -1) {
+            CardNum1 = card;
+        } else {
+            CardNum2 = card;
+        }
+        CardClickCount++;
+
+        console.log(CardNum1);
+        console.log(CardNum2);
+
+        if (CardClickCount === 2) {
+            if (CardNum1 === CardNum2) {
+                console.log("true");
+                CardMatchCount++; //카운터 header 컴포넌트에 전달하기
+            } else {
+                console.log("false");
+                //카드 다시 false -> card 컴포넌트에 전달하기
+            };
+
+            CardNum1 = -1;
+            CardNum2 = -1;
+            CardClickCount = 0;
+        };
+
+
+
     };
 
     return (
         <>
         <CardWrapper className="cardgame">
             {cards1.map((card, index) => (
-                <Card key={index} image={`/img${card}.jpeg`} onClick={() => handleCardClick(index)} />
+                <Card key={index} image={`/img${card}.jpeg`} onClick={() => handleCardClick(card)}/>
             ))}
         </CardWrapper>
         
         <CardWrapper className="cardgame">
             {cards2.map((card, index) => (
-                <Card key={index} image={`/img${card}.jpeg`} onClick={() => handleCardClick(index)} />
+                <Card key={index} image={`/img${card}.jpeg`} onClick={() => handleCardClick(card)} />
             ))}
         </CardWrapper>
         </>
