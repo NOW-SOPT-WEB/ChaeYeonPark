8f6c9f68f565f87c825b2638b111ee5e8d40c99d
chaeyeon <ccyy1029@naver.com>
2024-05-10 19:35:55 +0900

    style: main 페이지 스타일 설정

--- week4/login-project/src/pages/Mainpage.jsx
+++ week4/login-project/src/pages/Mainpage.jsx
@@ -1,4 +1,5 @@
 import mainCat from "../assets/img/maincat.jpeg"
+import styled from "styled-components";
 import { useParams, useNavigate } from "react-router-dom"
 
 const MainPage = () => {
@@ -15,16 +16,52 @@ const MainPage = () => {
     }
 
    return (
-    <div>
-        <h1>MainPage</h1>
-        <img src={mainCat} alt="메인 고양이" />
-        <div>
-            <button onClick={handleMypage}>내 정보</button>
-            <button onClick={handleSignup}>회원가입</button>
-        </div>
-    </div>
+    <Layout>
+        <h1>메인 페이지</h1>
+        <ImageWapper src={mainCat} alt="메인 고양이" />
+        <p>고양이 귀엽지요 🐈</p>
+        <ButtonContainer>
+            <ButtonWapper onClick={handleMypage}>내 정보</ButtonWapper>
+            <ButtonWapper onClick={handleSignup}>회원가입</ButtonWapper>
+        </ButtonContainer>
+    </Layout>
    )
 
 }
 
-export default MainPage
+export default MainPage;
+
+const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10rem;
+    height: 45rem;
+    background-color: #C5DCFF;
+    border-radius: 5rem;
+`
+
+const ImageWapper = styled.img`
+    width: 20rem;
+    border-radius: 1rem;
+`
+
+const ButtonContainer = styled.div`
+    margin: 1rem;
+`
+
+const ButtonWapper = styled.button`
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    margin: 1rem;
+    border: none;
+    border-radius: 1rem;
+    color: white;
+    background-color: #66A3FF;
+    font-size: 1.3rem;
+    font-weight: bold;
+
+    &:hover {
+        background-color: #2A7FFF;
+    }
+`
