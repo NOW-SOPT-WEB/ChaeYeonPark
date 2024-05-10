8f1538630145285c101a9182e53aae7acfefc17b
chaeyeon <ccyy1029@naver.com>
2024-05-10 14:32:32 +0900

    feat: 로그인 & 회원가입 api 연결

--- week4/login-project/src/pages/Loginpage.jsx
+++ week4/login-project/src/pages/Loginpage.jsx
@@ -1,25 +1,63 @@
 import { useState } from "react";
 //import styled form "styled-components";
-
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const LoginPage = () => {
+
+    const [userId, setUserId] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleLogin = async() => {
+        try {
+            const response = await axios.post("http://34.64.233.12:8080/member/login", {
+                authenticationId : userId,
+                password : userPassword
+            });
+
+            // 서버 확인
+            console.log(response.data);
+
+            // 서버 통신 성공
+            if (response.data.success) {
+                alert(response.data.message); // 로그인 성공 메시지 표시
+                navigate("/main");
+            } else {
+                alert(response.data.message); // 로그인 실패한 경우 서버에서 받은 에러 메시지 표시
+            } 
+        } catch (error) { // 서버 통신 실패
+            console.error("Error: ",error);
+            if (error.response && error.response.data) {
+                alert(error.response.data.message);
+            }
+        }
+    }
 
     return (
         <div>
             <h1>Login</h1>
-            <img src="./assets/cat.jpg" alt="cat" />
+            <img src="../assets/pink.jpg" alt="pink" />
             <div>
                 <p>ID</p>
-                <input type="text" />
+                <input 
+                    type="text"
+                    value={userId}
+                    onChange={(e) => setUserId(e.target.value)} />
             </div>
             <div>
                 <p>PW</p>
-                <input type="text" />
+                <input 
+                    type="text"
+                    value={userPassword}
+                    onChange={(e) => setUserPassword(e.target.value)} />
             </div>
 
             <div>
-                <button>로그인</button>
-                <button>회원가입</button>
+                <button onClick={handleLogin}>로그인</button>
+                <Link to="Signup">회원가입</Link>
             </div>
             
         </div>
--- week4/login-project/src/pages/Signuppage.jsx
+++ week4/login-project/src/pages/Signuppage.jsx
@@ -1,6 +1,70 @@
+import { useState } from "react";
+import axios from "axios";
+
 const SignupPage = () => {
+    const [userId, setUserId] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [userNickname, setUserNickname] = useState("");
+    const [userPhoneNumber, setUserPhoneNumber] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSignup = async() => {
+        try {
+            const response = await axios.post("http://34.64.233.12:8080/member/join", {
+                authenticationId : userId,
+                password : userPassword,
+                nickname : userNickname,
+                phone : userPhoneNumber
+            });
+            console.log(response.data);
+            // 회원가입 성공 처리
+            if (response.data.success) {
+                alert(response.data.message); // 성공 메시지 표시
+            } else {
+                alert(response.data.message); // 실패한 경우 서버에서 받은 에러 메시지 표시
+            }
+        } catch (error) {
+            console.error("Error: ", error);
+            setError("회원가입에 실패했습니다.")
+        }
+    }
+
+
     return (
-        <h1>SignupPage</h1>
+        <>
+        <h1>회원가입 페이지</h1>
+        <div>
+            <p>ID</p>
+            <input
+                type="text"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)} />
+        </div>
+        <div>
+            <p>비밀번호</p>
+            <input
+                type="text"
+                value={userPassword}
+                onChange={(e) => setUserPassword(e.target.value)}  />
+        </div>
+        <div>
+            <p>닉네임</p>
+            <input
+                type="text"
+                value={userNickname}
+                onChange={(e) => setUserNickname(e.target.value)}  />
+        </div>
+        <div>
+            <p>전화번호</p>
+            <input
+                type="text"
+                value={userPhoneNumber}
+                onChange={(e) => setUserPhoneNumber(e.target.value)}  />
+        </div>
+
+        <button onClick={handleSignup}>회원가입 하기</button>
+        {error && <p>{error}</p>}
+        </>
     )
 
 }
