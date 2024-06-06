6f7e09222e5b0cdcbf81def55eee39bc6a2f3ead
Parkchaeyeon <ccyy1029@naver.com>
2024-06-06 16:09:54 +0900

    ⚡️ feat: 모든 질문 선택 가능하게

--- week7/src/component/checkType.jsx
+++ week7/src/component/checkType.jsx
@@ -1,58 +1,104 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SOPTI_LIST } from "../data";
 import { deleteObject } from "../utils/deleteObject";
 
 const CheckType = () => {
-    let selectSopti = SOPTI_LIST;
-
-    let selectBox;
+    const [selectSopti, setSelectSopti] = useState(SOPTI_LIST);
+    const [deleteValue, setDeleteValue] = useState('');
+    const [questionNumber, setQuestionNumber] = useState(0);
     const [click, setClick] = useState(0);
-    const [type, setType] = useState(0);
-    //const [time, setTime] = useState(0);
-    //const [lead, setLead] = useState(0);
-    //const [plan, setPlan] = useState(0);
+    const [question1, setQuestion1] = useState('대면 회의를 선호한다.');
+    const [question2, setQuestion2] = useState('비대면 회의를 선호한다.');
+    const [questionType, setQuestionType] = useState('meet');
+    const [nextButton, setNextButton] = useState('다음으로');
 
-    const handleClickTypeD = () => {
+    //번호에 따라 질문 내용 바꾸기
+    useEffect(() => {
+        switch (questionNumber) {
+            case 0:
+                setQuestionType('meet')
+                setQuestion1('대면 회의를 선호한다.')
+                setQuestion2('비대면 회의를 선호한다.')
+                break
+            case 1:
+                setQuestionType('time')
+                setQuestion1('주로 날이 밝을 때 작업한다.')
+                setQuestion2('주로 어두울 때 작업한다.')
+                break
+            case 2:
+                setQuestionType('lead')
+                setQuestion1('팀장을 선호하는 편이다.')
+                setQuestion2('팀원을 선호하는 편이다.')
+                break
+            case 3:
+                setQuestionType('plan')
+                setQuestion1('계획적으로 순서를 정해서 작업하는 편이다.')
+                setQuestion2('이것 저것 생각나는대로 작업하는 편이다.')
+                setNextButton('결과보기')
+                break
+        }
+    }, [questionNumber])
+
+
+    const handleClickType1 = () => {
         setClick(1)
     };
 
-    const handleClickTypeB = () => {
+    const handleClickType2 = () => {
         setClick(2)
     };
 
     const handleClickType = () => {
-        if (click === 1) {setType(1)}
-        if (click === 2) {setType(2)}
+        if (click === 1) {
+            switch (questionType) {
+                case 'meet': 
+                    setDeleteValue('B');
+                    break
+                case 'time':
+                    setDeleteValue('N');
+                    break
+                case 'lead':
+                    setDeleteValue('F');
+                    break
+                case 'plan':
+                    setDeleteValue('P');
+                    break
+            }
+        }
+        if (click === 2) {
+            switch (questionType) {
+                case 'meet': 
+                    setDeleteValue('D');
+                    break
+                case 'time':
+                    setDeleteValue('M');
+                    break
+                case 'lead':
+                    setDeleteValue('L');
+                    break
+                case 'plan':
+                    setDeleteValue('J');
+                    break
+            }
+        }
+
+        if (deleteValue) {
+            const newSopti = {...selectSopti};
+            deleteObject(newSopti, questionType, `${deleteValue}`);
+            setSelectSopti(newSopti);
+            setQuestionNumber(prev => prev + 1);
+            console.log(selectSopti);
+        }
     };
 
-    switch (type) {
-        case 0:
-        selectBox = 
-            <div>
-                <div>
-                    <button onClick={handleClickTypeD}>대면 회의를 선호한다.</button>
-                    <button onClick={handleClickTypeB}>비대면 회의를 선호한다.</button>
-                </div>
-                <button onClick={handleClickType}>다음으로</button>
-            </div>
-        break
-
-        case 1:
-        //배열에서 type='B'인 애들을 제거하고, 다음 물음으로 넘어간다.
-        deleteObject(selectSopti, 'type', 'B')
-        console.log(selectSopti)
-        break
-
-        case 2:
-        //배열에서 type='D'인 애들을 제거하고, 다음 물음으로 넘어간다.
-        deleteObject(selectSopti, type, 'B')
-        console.log(selectSopti)
-        break
-    }
 
     return (
         <div>
-            {selectBox}
+            <div>
+                <button onClick={handleClickType1}> {question1}</button>
+                <button onClick={handleClickType2}> {question2}</button>
+            </div>
+            <button onClick={handleClickType}>{nextButton}</button>
         </div>
     )
 
--- week7/src/data.js
+++ week7/src/data.js
@@ -1,21 +1,21 @@
 export const SOPTI_LIST = {
     BMLP: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'M',
         lead: 'L',
         plan: 'P'
     },
     BMLJ: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'M',
         lead: 'L',
         plan: 'J'
     },
     BMFP: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'M',
         lead: 'F',
         plan: 'P'
@@ -23,91 +23,91 @@ export const SOPTI_LIST = {
 
     BMFJ: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'M',
         lead: 'F',
         plan: 'J'
     },
     BNLP: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'N',
         lead: 'L',
         plan: 'P'
     },
     BNLJ: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'N',
         lead: 'L',
         plan: 'J'
     },
     BNFP: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'N',
         lead: 'F',
         plan: 'P'
     },
     BNFJ: {
         id: 1,
-        type: 'B',
+        meet: 'B',
         time: 'N',
         lead: 'F',
         plan: 'J'
     },
     DMLP: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'M',
         lead: 'L',
         plan: 'P'
     },
     DMLJ: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'M',
         lead: 'L',
         plan: 'J'
     },
     DMFP: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'M',
         lead: 'F',
         plan: 'P'
     },
     DMFJ: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'M',
         lead: 'F',
         plan: 'J'
     },
     DNLP: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'N',
         lead: 'L',
         plan: 'P'
     },
     DNLJ: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'N',
         lead: 'L',
         plan: 'J'
     },
     DNFP: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'N',
         lead: 'F',
         plan: 'P'
     },
     DNFJ: {
         id: 1,
-        type: 'D',
+        meet: 'D',
         time: 'N',
         lead: 'F',
         plan: 'J'
