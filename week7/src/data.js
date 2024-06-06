8ee2561241f0c754ab796321f29933a7a99279ab
Parkchaeyeon <ccyy1029@naver.com>
2024-06-06 21:45:01 +0900

    📦chore: 이미지 파일 추가

--- week7/src/data.js
+++ week7/src/data.js
@@ -1,24 +1,29 @@
+
+
 export const SOPTI_LIST = {
     BMLP: {
         id: 1,
         meet: 'B',
         time: 'M',
         lead: 'L',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/BMLP.jpg'
     },
     BMLJ: {
         id: 1,
         meet: 'B',
         time: 'M',
         lead: 'L',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/BMLJ.jpg'
     },
     BMFP: {
         id: 1,
         meet: 'B',
         time: 'M',
         lead: 'F',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/BMFP.jpg'
     },
 
     BMFJ: {
@@ -26,90 +31,103 @@ export const SOPTI_LIST = {
         meet: 'B',
         time: 'M',
         lead: 'F',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/BMFJ.jpg'
     },
     BNLP: {
         id: 1,
         meet: 'B',
         time: 'N',
         lead: 'L',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/BNLP.jpg'
     },
     BNLJ: {
         id: 1,
         meet: 'B',
         time: 'N',
         lead: 'L',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/BNLJ.jpg'
     },
     BNFP: {
         id: 1,
         meet: 'B',
         time: 'N',
         lead: 'F',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/BNFP.jpg'
     },
     BNFJ: {
         id: 1,
         meet: 'B',
         time: 'N',
         lead: 'F',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/BNFJ.jpg'
     },
     DMLP: {
         id: 1,
         meet: 'D',
         time: 'M',
         lead: 'L',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/DMLP.jpg'
     },
     DMLJ: {
         id: 1,
         meet: 'D',
         time: 'M',
         lead: 'L',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/DMLJ.jpg'
     },
     DMFP: {
         id: 1,
         meet: 'D',
         time: 'M',
         lead: 'F',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/DMFP.jpg'
     },
     DMFJ: {
         id: 1,
         meet: 'D',
         time: 'M',
         lead: 'F',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/DMFJ.jpg'
     },
     DNLP: {
         id: 1,
         meet: 'D',
         time: 'N',
         lead: 'L',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/DNLP.jpg'
     },
     DNLJ: {
         id: 1,
         meet: 'D',
         time: 'N',
         lead: 'L',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/DNLJ.jpg'
     },
     DNFP: {
         id: 1,
         meet: 'D',
         time: 'N',
         lead: 'F',
-        plan: 'P'
+        plan: 'P',
+        image: 'assets/image/DNFP.jpg'
     },
     DNFJ: {
         id: 1,
         meet: 'D',
         time: 'N',
         lead: 'F',
-        plan: 'J'
+        plan: 'J',
+        image: 'assets/image/DNFJ.jpg'
     }
 }
